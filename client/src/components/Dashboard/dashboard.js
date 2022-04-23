78d766dd5e3a41a4a2e64efa79a040c79876768e
Daniel Sousa <dabs.sousa@gmail.com>
2022-04-23 18:04:58 +0100

    added routes

--- client/src/components/Dashboard/dashboard.js
+++ client/src/components/Dashboard/dashboard.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect } from 'react'
 import './dashboardStyle.css';
+import { BrowserRouter, HashRouter, Routes, Route, NavLink } from 'react-router-dom';
 
 import { getUserEmissions } from '../../service';
 import { UserForm } from '../Form/userForm';
+import { HomePg } from '../HomePage/homePg';
+import { NeutralizePg } from '../NeutralizePage/neutralizePg';
 
 export function Dashboard() {
 
@@ -17,17 +20,25 @@ export function Dashboard() {
 
   return (
     <>
-      <div>dashboard</div>
-      <div className='layout-container'>
-        <ul className='header-navbar'>
-          <li><a href="/">Home</a></li>
-          <li><a href="/profile">Profile</a></li>
-          <li><a href="/neutralize">Become Carbon Neutral</a></li>
-        </ul>
-        <div className='profile-container'>
-          <UserForm setCardsList={setCardsList}></UserForm>
+      <BrowserRouter>
+      {/* <HashRouter>*/}
+        <div>dashboard</div>
+        <div className='layout-container'>
+          <ul className='header-navbar'>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/profile">Profile</NavLink></li>
+            <li><NavLink to="/neutralize">Become Carbon Neutral</NavLink></li>
+          </ul>
+          <div className='content-container'>
+            <Routes>
+              <Route path='/' element={<HomePg />} />
+              <Route path='/profile' element={<UserForm setCardsList={setCardsList} />} />
+              <Route path='/neutralize' element={<NeutralizePg />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      {/* </HashRouter> */}
+      </BrowserRouter>
 
     </>
   )
